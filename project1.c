b594944b820c490b41324e8556093ca2136d99d8
Mikkal Mcnulty <mcnultymikkal@gmail.com>
2018-05-22 10:16:10 -0700

    handle equal bases to equal bases

--- project1.c
+++ project1.c
@@ -9,7 +9,7 @@ char** copyArrayOfRange(char* inputArray[], int start, int end)
 {
     int i;
     char **result = (char **) malloc((end-start)*sizeof(char*));
-    for (i = start; i < end; i) 
+    for (i = start; i < end; i +=1) 
     {
         result[i-start] = (char *) malloc(sizeof(char) * strlen(inputArray[i]));
         sprintf(result[i-start], "%s", inputArray[i]);
@@ -37,7 +37,9 @@ char** binaryToDecimal (char* inputArray[], int interval)
 int main(int argc, char *argv[])
 {
     char **result;
+    char **copy;
     int i;
+
     if (argc < 4)
     {
         printf("ERROR: You need an argument.\n");
@@ -51,10 +53,16 @@ int main(int argc, char *argv[])
 
         printf("Starting base %c \n",  startingBase);
         printf("Ending base %c \n", endingBase);
+        printf("Size %i \n", argc);
 
         if(startingBase == 'd') {
             if(endingBase == 'd') {
-                printf("return unmutated array");
+                copy = copyArrayOfRange(argv, 3, argc);
+                printf("\n");
+                for (i = 0; i < argc - 3; i += 1)
+                {
+                    printf("%s ", copy[i]);
+                }
             }
             else if (endingBase == 'h') {
                 printf("dec to hex");
@@ -70,13 +78,18 @@ int main(int argc, char *argv[])
         }
 
         else if(startingBase == 'h') {
-            if (endingBase == 'd')
+            if (endingBase == 'h')
+            {
+                copy = copyArrayOfRange(argv, 3, argc);
+                printf("\n");
+                for (i = 0; i < argc - 3; i += 1)
+                {
+                    printf("%s ", copy[i]);
+                }
+            }
+            else if (endingBase == 'd')
             {
                 printf("return unmutated array");
-            }
-            else if (endingBase == 'h')
-            {
-                printf("dec to hex");
             }
             else if (endingBase == 'b')
             {
@@ -90,17 +103,22 @@ int main(int argc, char *argv[])
         }
 
         else if(startingBase == 'b') {
-            if (endingBase == 'd')
+            if (endingBase == 'b')
+            {
+                copy = copyArrayOfRange(argv, 3, argc);
+                printf("\n");
+                for (i = 0; i < argc - 3; i += 1)
+                {
+                    printf("%s ", copy[i]);
+                }
+            }
+            else if (endingBase == 'd')
             {
                 printf("return unmutated array");
             }
             else if (endingBase == 'h')
             {
                 printf("dec to hex");
-            }
-            else if (endingBase == 'b')
-            {
-                printf("dec to bin");
             }
             else
             {
@@ -113,7 +131,7 @@ int main(int argc, char *argv[])
             printf("ERROR: Improper base input.\n");
             return 1;
         }
-
+              
         // result = binaryToDecimal(copyArrayOfRange(argv, 0, argc), argc);
         // for (i = 0; i < argc; i += 1)
         // {
@@ -123,13 +141,4 @@ int main(int argc, char *argv[])
         // free(result);
         // return 0;
     }
-    // for (i = 1; i < argc; i += 1)
-    // {
-    //     char *pos = argv[i];
-    //     while (*pos != '\0')
-    //     {
-    //         printf("%c\n", *(pos++));
-    //     }
-    //     printf("\n");
-    // }
 }
